c2f51aa7c7f75fe2cdb44dabc59b94dd65a5b88b
yohsuke-kurita <104214314+yohsuke-kurita@users.noreply.github.com>
2022-04-28 17:35:28 +0900

    feat: add the 4th message

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
 exports.printMsg = function () {
   console.log("This is a message from the demo package");
   console.log("3rd: This is also a message from the demo package");
+  console.log("4th: This is also a message from the demo package");
 };
